6ed364b5f544eca2225de09884983b1bcb9e7634
RyanD177 <w0477770@campus.nscc.ca>
2023-12-18 23:30:33 -0400

    starter

---
+++ crypto_prices/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import CryptoPrices from './prices';
+
+function App() {
+  return (
+    <div className="App">
+      <CryptoPrices ></CryptoPrices>
+    </div>
+  );
+}
+
+export default App;
---
+++ crypto_prices/src/prices.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const CRYPTO_PRICES_API_BASE_URL =
+  'https://api.frontendexpert.io/api/fe/cryptocurrencies';
+  
+export default function CryptoPrices() {
+
+
+  return (
+    <>
+        <h1> Hello</h1>
+    </>
+  );
+}
